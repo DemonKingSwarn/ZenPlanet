f394c874fad024c976f852dbcd973327d6472ab2
DemonKingSwarn <rockingswarn@gmail.com>
2023-03-22 22:15:25 +0530

    Chore: core mechanics done

---
+++ Assets/Scripts/RandomPatrol.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomPatrol : MonoBehaviour
+{
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+    Vector2 targetPosition;
+
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	targetPosition = GetRandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Vector2)transform.position != targetPosition)
+	{
+		transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+	} else {
+		targetPosition = GetRandomPosition();
+	}
+    }
+
+    Vector2 GetRandomPosition()
+    {
+	float randomX = Random.Range(minX, maxX);
+	float randomY = Random.Range(minY, maxY);
+	return new Vector2(randomX, randomY);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+	if(collision.tag == "Planets")
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+    }
+}
